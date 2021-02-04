b441f225e3ae75ce39356687092e7d73993d10e0
unknown <victorchemla@epita.fr>
2021-02-04 19:42:14 +0100

    [TEAM] Add the button team present the terrain

---
+++ Rockets/app/src/main/java/com/example/rockets/MainTeam.java
@@ -0,0 +1,14 @@
+package com.example.rockets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainTeam extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_team);
+    }
+}
