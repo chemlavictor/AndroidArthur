1b42459d61019a6723b04e19b96c967b25610724
unknown <victorchemla@epita.fr>
2021-02-05 11:34:54 +0100

    [CONTACTUS] Add the button contact us with the link

--- Rockets/app/src/main/java/com/example/rockets/MainActivity.java
+++ Rockets/app/src/main/java/com/example/rockets/MainActivity.java
@@ -3,23 +3,35 @@ package com.example.rockets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
+    private Button buttonTeam;
+    private Button buttonContactUS;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.button6);
-        button.setOnClickListener(new View.OnClickListener() {
+        buttonTeam = (Button) findViewById(R.id.button6);
+        buttonTeam.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openTeam();
+            }
+        });
+
+        buttonContactUS = (Button) findViewById(R.id.button9);
+        buttonContactUS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://rocketsnationfrance.com/"));
+                startActivity(browserIntent);
             }
         });
     };
